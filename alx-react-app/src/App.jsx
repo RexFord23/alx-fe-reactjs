fcc7c222ce11511472cb3869c50c9306c8c746f4
Rexford Oppong <Rexforddickson@gmail.com>
2026-01-17 23:46:40 +0000

    Add App component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
+      <WelcomeMessage />
     </>
   )
 }
