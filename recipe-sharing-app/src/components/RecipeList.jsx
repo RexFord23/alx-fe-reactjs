2c95fba50ded06b2f6949c723dddfba5e90a230b
Rexford Oppong <Rexforddickson@gmail.com>
2026-01-25 14:51:54 +0000

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
 import { useRecipeStore } from "../store/recipeStore";
 
 const RecipeList = () => {
-    const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
-    return (
-        <div>
-           <h2>Recipes</h2>
-           {recipes.lenght === 0 && <p>No Recipes yet.</p>}
-
-           {recipes.map((recipe) => (
-            <div key={recipe.id}>
-                <h3>{recipe.title}</h3>
-                <p>{recipe.description}</p>
-            </div>
-           ))}
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          </h3>
+          <p>{recipe.description}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecipeList;
