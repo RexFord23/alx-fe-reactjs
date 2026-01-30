7294f453390488a722b9cacd1ae020d1b42933d8
Rexford <rexfordampadu@live.com>
2026-01-30 16:49:29 +0000

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,22 +5,18 @@ import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        }
+      />
+      <Route path="/recipes/:id" element={<RecipeDetails />} />
+    </Routes>
   );
 }
 
