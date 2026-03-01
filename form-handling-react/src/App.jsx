98d4128331ca5ad0744c36c261ada55126831243
Rexford <rexfordampadu@live.com>
2026-03-01 09:41:53 +0000

    Implementing Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,12 @@
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import FormikForm from "./components/formikForm"; 
 
 function App() {
   return (
-    <>
+    <div>
       <RegistrationForm />
-      <hr />
-      <formikForm />
-    </>
+      <FormikForm />  
+    </div>
   );
 }
 
